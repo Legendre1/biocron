456dbca4092e7653f6486927becfbba0b3b62ac8
JGolden <jonlgolden@gmail.com>
2024-11-08 18:28:18 -0700

    Header links

--- src/components/biocron/header.tsx
+++ src/components/biocron/header.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import logo from './assets/logo-placeholder-image.png'
+import { Link } from 'react-router-dom';
 
 const Header: React.FC  = () => {
 
@@ -7,10 +8,10 @@ const Header: React.FC  = () => {
   return (
     <div className="w-screen flex place-content-center"> 
       <img className='mx-2 size-16' src={logo}></img>
-      <h1 className='mx-2 text-slate-900 dark:text-white'>Home</h1>
-      <h1 className='mx-2 text-slate-900 dark:text-white'>Bio</h1>
-      <h1 className='mx-2 text-slate-900 dark:text-white'>Portfolio</h1>
-      <h1 className='mx-2 text-slate-900 dark:text-white'>Contact</h1>
+      <Link className='mx-2 text-slate-900 dark:text-white text-2xl' to="/">Home</Link>
+      <Link className='mx-2 text-slate-900 dark:text-white text-2xl' to="/bio">Bio</Link>
+      <Link className='mx-2 text-slate-900 dark:text-white text-2xl' to="/portfolio">Portfolio</Link>
+      <Link className='mx-2 text-slate-900 dark:text-white text-2xl' to="/contact">Contact</Link>
     </div>
   );
 };
