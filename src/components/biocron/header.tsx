96af205e42211c490ceb581586dbee710aa17e11
JGolden <jonlgolden@gmail.com>
2024-11-11 20:32:44 -0700

    The header flexing is working somewhat correctly.

--- src/components/biocron/header.tsx
+++ src/components/biocron/header.tsx
@@ -6,10 +6,12 @@ const Header: React.FC  = () => {
 
 
   return (
-    <div className="w-screen flex place-content-center"> 
-      <img className='mx-2 size-16' src={logo}></img>
-      <Link className='mx-2 text-slate-900 dark:text-white text-2xl' to="/">Home</Link>
-      <Link className='mx-2 text-slate-900 dark:text-white text-2xl' to="/bio">Bio</Link>
+    <div className="w-screen flex flex-row items-center bg-blue-900"> 
+     <div className='basis-24'></div>
+      <img className='mx-2 size-16 ' src={logo}></img>
+      <div className='basis-1/2'></div>
+      <Link className='mx-2 text-slate-900 dark:text-white text-2xl shrink' to="/">Home</Link>
+      <Link className='mx-2 text-slate-900 dark:text-white text-2xl shrink' to="/bio">Bio</Link>
       <Link className='mx-2 text-slate-900 dark:text-white text-2xl' to="/portfolio">Portfolio</Link>
       <Link className='mx-2 text-slate-900 dark:text-white text-2xl' to="/contact">Contact</Link>
     </div>
