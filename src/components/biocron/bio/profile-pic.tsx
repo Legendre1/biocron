b0fa19cf210ea3f9626212fc862339f0804aee58
JGolden <jonlgolden@gmail.com>
2024-11-18 22:03:52 -0700

    Aspect on safari is bad for profile pic

--- src/components/biocron/bio/profile-pic.tsx
+++ src/components/biocron/bio/profile-pic.tsx
@@ -6,7 +6,7 @@ const ProfilePic: React.FC  = () => {
 
   return (
     <div className="flex flex-row bg-gray-900 w-80 h-96 rounded-lg mx-4"> 
-      <img className='w-80  rounded-lg m-8' src={laios}></img>
+      <img className='h-80 rounded-lg m-8' src={laios}></img>
     </div>
   );
 };
