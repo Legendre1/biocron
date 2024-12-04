b4df6ab1c043988c345dfaf51f6b3a9c6cfa4dc0
JGolden <jonlgolden@gmail.com>
2024-12-03 22:22:12 -0700

    Bio fixes

--- src/components/biocron/bio/bio-blurb.tsx
+++ src/components/biocron/bio/bio-blurb.tsx
@@ -3,21 +3,19 @@ import FramePanel from '../frame-panel';
 
 const BioBlurb: React.FC  = () => {
   return (
-    <div className='w-full px-4'>
-      <FramePanel classes=" h-min mt-4 mx-4 -mb-2 p-4"> 
+    <div className='w-full px-2'>
+      <FramePanel isButton={false} onClick={() => {}} classes=" h-min mt-4 mx-4 -mb-2 p-4"> 
       <div className='flex flex-col items-center justify-center h-fit'>
         <div className='text-center text-4xl my-2 font-title'>Bio</div>
+        <div className='h-6'></div>
         <div className='text-center text-xl my-2 font-body'>
-          Hello friends! I am a veteran of mobile and web game development, with a recent emphasis on frontend web development. I have been making games in Unity for the past decade,
-          including titles that reached 100M+ downloads on the mobile app stores.
-        </div>
-        <div className='text-center text-xl my-2 font-body'>
-          I founded Aureus Studios to make mobile and console games under my own label. Our inaugural title, hashPICS, is available on
-          the <a href="https://apps.apple.com/us/app/hashpics/id1619576843">iOS App Store</a>, the <a href="https://play.google.com/store/apps/details?id=com.aureusstudios.hashpics&hl=en">Google Play Store</a>
-          , or as a <a href="https://hashpics.netlify.app">prototype React web app</a>
-        </div>
-        <div className='text-center text-xl my-2 font-body'>
-          My personal interests include 3rd and 4th generation video games and consoles, vinyl movie soundtracks, and cinema generally.
+          Hi, friends! I am a veteran of mobile and web game development with a recent focus on frontend web technologies. Over the past decade, I have been developing games in Unity, including several titles that have achieved over 100 million downloads on the mobile app stores.
+          </div>
+          <div className='text-center text-xl my-2 font-body'>
+          I am also the founder of Aureus Studios, where I bring my creative visions to life by crafting mobile and console games, both as a solo developer and as leader of a small indie team.
+          </div>
+          <div className='text-center text-xl my-2 font-body'>
+          Outside of work, I am passionate about 3rd and 4th generation video games and consoles, collecting vinyl movie soundtracks, and exploring the world of cinema.
         </div>
       </div>  
     </FramePanel>
--- src/components/biocron/screens/bio-screen.tsx
+++ src/components/biocron/screens/bio-screen.tsx
@@ -2,8 +2,6 @@
 import Header from './../header';
 import ProfilePic from './../bio/profile-pic';
 import BioBlurb from './../bio/bio-blurb';
-import ExpBlurb from './../bio/exp-blurb';
-import QualificationBlurb from './../bio/qualification-blurb';
 
 function BioScreen() {
 
@@ -14,10 +12,6 @@ function BioScreen() {
       <div className='flex place-content-center items-center w-screen mt-32 flex-col md:flex-row'> 
         <ProfilePic />
         <BioBlurb />
-      </div>
-      <div className='flex place-content-center items-center w-screen mt-6 flex-col md:flex-row'>
-        <ExpBlurb />
-        <QualificationBlurb />
       </div>
     </div>
   )
